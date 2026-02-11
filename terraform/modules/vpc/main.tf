b83d1aabcd4e4d168c5a75fbeee4efad9d4f5ded
kaushikpatel09-crest <kaushik.p@crestinfosystems.com>
2026-02-11 09:19:59 +0530

    updated FE security port

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -182,15 +182,15 @@ resource "aws_security_group" "ecs_frontend" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    from_port       = 80
-    to_port         = 80
+    from_port       = 3000
+    to_port         = 3000
     protocol        = "tcp"
     security_groups = [aws_security_group.alb_public.id]
   }
 
-  ingress {
-    from_port       = 443
-    to_port         = 443
+ ingress {
+    from_port       = 80
+    to_port         = 80
     protocol        = "tcp"
     security_groups = [aws_security_group.alb_public.id]
   }
