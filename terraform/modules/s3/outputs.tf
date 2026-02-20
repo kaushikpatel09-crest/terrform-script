d696c395af1f2e5d201c8a2984126a5af7c0216c
kaushikpatel09-crest <kaushik.p@crestinfosystems.com>
2026-02-20 15:45:26 +0530

    added bucket name

--- terraform/modules/s3/outputs.tf
+++ terraform/modules/s3/outputs.tf
@@ -28,12 +28,18 @@ output "processed_bucket_arn" {
   value       = aws_s3_bucket.processed.arn
 }
 
+output "image_search_bucket_arn" {
+  description = "Image search S3 bucket ARN"
+  value       = aws_s3_bucket.image_search.arn
+}
+
 output "all_bucket_arns" {
   description = "All S3 bucket ARNs"
   value = [
     aws_s3_bucket.landing.arn,
     aws_s3_bucket.raw.arn,
-    aws_s3_bucket.processed.arn
+    aws_s3_bucket.processed.arn,
+    aws_s3_bucket.image_search.arn
   ]
 }
 
